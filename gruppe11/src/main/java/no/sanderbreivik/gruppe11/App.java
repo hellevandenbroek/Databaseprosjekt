caddfa72ae079936ee941fe224d27f8769e1b01a
Sander Breivik <sanderbreivik@Sanders-MacBook-Pro.local>
2018-03-13 14:39:27 +0100

    Added maven project

---
+++ gruppe11/src/main/java/no/sanderbreivik/gruppe11/App.java
@@ -0,0 +1,13 @@
+package no.sanderbreivik.gruppe11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
