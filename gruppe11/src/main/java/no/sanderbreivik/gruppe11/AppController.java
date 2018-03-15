bce1bcaf095fe2f7f5d61076024fdf61f7b5381d
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-03-15 10:12:10 +0100

    Added missed add

---
+++ gruppe11/src/main/java/no/sanderbreivik/gruppe11/AppController.java
@@ -0,0 +1,10 @@
+package no.sanderbreivik.gruppe11;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
