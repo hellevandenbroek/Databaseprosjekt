ce5423d9c7ec7885a387dfa59e956d827cdfec35
= <=>
2018-03-15 13:03:56 +0100

    added workout.fxml and started on controller

---
+++ gruppe11/src/main/java/ui/addWorkoutController.java
@@ -0,0 +1,12 @@
+package ui;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class addWorkoutController {
+
+	
+	@FXML Button addExercise, addSelected, addSelectedAndClose;
+	
+}
