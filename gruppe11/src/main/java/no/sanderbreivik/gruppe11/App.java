e50b342fb2b802ca0d9653b61290f663011a0aac
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-03-15 10:11:35 +0100

    Moved things in the same packages

--- gruppe11/src/main/java/no/sanderbreivik/gruppe11/App.java
+++ gruppe11/src/main/java/no/sanderbreivik/gruppe11/App.java
@@ -1,13 +1,26 @@
 package no.sanderbreivik.gruppe11;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    
+    }
+
+    public void start (Stage stage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Treningsapp");
+		stage.show();
     }
 }
+
