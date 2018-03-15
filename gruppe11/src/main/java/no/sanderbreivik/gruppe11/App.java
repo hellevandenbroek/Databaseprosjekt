a84f572d2c4c6f7789993ae7945fe5d62bd59e57
Sander Breivik <sandeb@stud.ntnu.no>
2018-03-15 10:16:15 +0100

    Removed merge conflict

--- gruppe11/src/main/java/no/sanderbreivik/gruppe11/App.java
+++ gruppe11/src/main/java/no/sanderbreivik/gruppe11/App.java
@@ -1,39 +1,23 @@
 package no.sanderbreivik.gruppe11;
 
-<<<<<<< Updated upstream
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-=======
-import db_connection.ConnectService;
->>>>>>> Stashed changes
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-<<<<<<< Updated upstream
-    public static void main( String[] args ){
-    
-    }
+public class App {
+	public static void main(String[] args) {
+	}
 
-    public void start (Stage stage) throws Exception{
+	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.setTitle("Treningsapp");
 		stage.show();
-=======
-    public static void main( String[] args )
-    {
-    		ConnectService c = new ConnectService();
-        if (condition) {
-			
-		}
->>>>>>> Stashed changes
-    }
+	}
 }
-
