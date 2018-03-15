0f20263beb3b02aca39cd1b5c46adb426d34465a
= <=>
2018-03-15 10:12:44 +0100

    sander made it works

--- gruppe11/src/main/java/Queries/Valid_user.java
+++ gruppe11/src/main/java/Queries/Valid_user.java
@@ -6,61 +6,119 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import db_connection.QueryImporter;
 
 public class Valid_user extends QueryImporter{
-	static Connection con;
-	static Statement stmt;
-	public static void getConnected() throws InstantiationException, IllegalAccessException {
-		 System.out.println("Howwowo");
-		try {
-		  Class.forName("com.mysql.jdbc.Driver").newInstance();
-		  String url = "jdbc:mysql://mysqladmin.stud.ntnu.no/sandeb_dbProsjekt";
-		  String user = "simonnb";
-		  String pw = "acerSwift";
-		  Connection init = DriverManager.getConnection(url,user,pw);
-		  System.out.println(init);
-		  System.out.println("Tilkoblingen fungerte.");
-		  Statement message = init.createStatement() ;
-		  stmt = message;
-		  con = init;
-		  } catch (SQLException ex) {
-			  System.out.println("H22222");
-		    System.out.println("Tilkobling feilet: "+ex.getMessage());
-		  } catch (ClassNotFoundException other) {
-			  System.out.println("H3333");
-		    System.out.println("Feilet under driverlasting: "+other.getMessage());
-		  } finally {
-		    try {
-		      if (con !=  null) con.close();
-		    } catch (SQLException ex) {
-		      System.out.println("Epic fail: "+ex.getMessage());
-		    }
-		  }
+	/** The name of the MySQL account to use (or empty for anonymous) */
+	private final String userName = "trulsabe";
 
+	/** The password for the MySQL account (or empty for anonymous) */
+	private final String password = "zenbook4";
+
+	/** The name of the computer running MySQL */
+	private final String serverName = "mysql.stud.ntnu.no";
+
+	/** The port of the MySQL server (default is 3306) */
+	private final int portNumber = 3306;
+
+	/** The name of the database we are testing with (this default is installed with MySQL) */
+	private final String dbName = "sandeb_dbProsjekt";
+	
+	/** The name of the table we are testing with */
+	private final String tableName = "JDBC_TEST";
+	
+	/**
+	 * Get a new database connection
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		Properties connectionProps = new Properties();
+		connectionProps.put("user", this.userName);
+		connectionProps.put("password", this.password);
+
+		conn = DriverManager.getConnection("jdbc:mysql://"
+				+ this.serverName + ":" + this.portNumber + "/" + this.dbName,
+				connectionProps);
+
+		return conn;
+	}
+
+	/**
+	 * Run a SQL command which does not return a recordset:
+	 * CREATE/INSERT/UPDATE/DELETE/DROP/etc.
+	 * 
+	 * @throws SQLException If something goes wrong
+	 */
+	public boolean executeUpdate(Connection conn, String command) throws SQLException {
+	    Statement stmt = null;
+	    try {
+	        stmt = conn.createStatement();
+	        stmt.executeUpdate(command); // This will throw a SQLException if it fails
+	        return true;
+	    } finally {
+
+	    	// This will run whether we throw an exception or not
+	        if (stmt != null) { stmt.close(); }
+	    }
 	}
 	
-	 
-	public static Boolean validUser (String userID) {
-		String query_String = "SELECT COUNT(*) FROM bruker WHERE id = "+ userID + "";
-		ResultSet rs = null;
+	/**
+	 * Connect to MySQL and do some stuff.
+	 */
+	public void run() {
+
+		// Connect to MySQL
+		Connection conn = null;
 		try {
-			rs = stmt.executeQuery(query_String);
-			System.out.println(rs);
+			conn = this.getConnection();
+			System.out.println("Connected to database");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.out.println("ERROR: Could not connect to the database");
 			e.printStackTrace();
+			return;
 		}
-		if(Integer.parseInt(rs.toString())>0) {
-			return true;
+
+		// Create a table
+		try {
+		    String createString =
+			        "CREATE TABLE " + this.tableName + " ( " +
+			        "ID INTEGER NOT NULL, " +
+			        "NAME varchar(40) NOT NULL, " +
+			        "STREET varchar(40) NOT NULL, " +
+			        "CITY varchar(20) NOT NULL, " +
+			        "STATE char(2) NOT NULL, " +
+			        "ZIP char(5), " +
+			        "PRIMARY KEY (ID))";
+			this.executeUpdate(conn, createString);
+			System.out.println("Created a table");
+	    } catch (SQLException e) {
+			System.out.println("ERROR: Could not create the table");
+			e.printStackTrace();
+			return;
 		}
-		else {
-			return false;
+		
+		// Drop the table
+		try {
+		    String dropString = "DROP TABLE " + this.tableName;
+			this.executeUpdate(conn, dropString);
+			System.out.println("Dropped the table");
+	    } catch (SQLException e) {
+			System.out.println("ERROR: Could not drop the table");
+			e.printStackTrace();
+			return;
 		}
 	}
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-		getConnected();
-		System.out.println(validUser("1"));
+	
+	/**
+	 * Connect to the DB and do some stuff
+	 */
+	public static void main(String[] args) {
+		Valid_user app = new Valid_user();
+		app.run();
 	}
 }
