b0e60e3742dfc3c6c1720ac260603595c66c95bb
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-03-15 15:02:16 +0100

    Queries n shit

--- gruppe11/src/main/java/ui/App.java
+++ gruppe11/src/main/java/ui/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.setTitle("Treningsapp");
